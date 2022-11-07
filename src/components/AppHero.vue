7e07816b2d31d2aa700d8cf7c33082a688ed863e
gioanbatan <giovannibassani@hotmail.com>
2022-11-07 17:12:43 +0100

    Add: Title and Jumbotron

---
+++ src/components/AppHero.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    data () {
+        return {
+            jumboText: "Questo è il mio primo jumbotron in Vue + Vite"
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="jumbotron">
+        <span class="jt-text">{{jumboText}}</span>
+    </div>
+</template>
+
+<style scoped>
+    .jumbotron {
+        width: 100%;
+        height: 500px;
+        background-image: url("../assets/pizza_pexels.jpg");
+        background-size: cover;
+        background-position: center;
+        padding: 1.5rem
+    }
+
+    .jt-text {
+        display: block;
+        text-align: end;
+        font-size: 3rem;
+        color: white;
+        text-shadow: 2px 2px black;
+    }
+</style>
